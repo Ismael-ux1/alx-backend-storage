58c7572505e88d2a594f200b874ad921d3b7981f
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-01-21 22:05:29 +0000

    updated Optimize simple search

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -2,10 +2,7 @@
 ALTER TABLE names ADD COLUMN first_letter CHAR(1);
 
 -- Update the new column with the first letter of the name
-UPDATE names SET first_letter = LEFT(name, 1);
+UPDATE names SET first_letter = SUBSTRING(name, 1, 1);
 
 -- Create the index on the new column
-CREATE INDEX idx_name_first ON names (first_letter);
-
--- Verify the index is created
-SHOW INDEX FROM names;
+CREATE INDEX idx_name_first ON names(first_letter);
