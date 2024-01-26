29dc150919ff1f2918e9fb9886a18cc688bb028d
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-01-26 14:35:39 -0800

    update topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -11,25 +11,10 @@ def update_topics(mongo_collection, name, topics):
     topics: List of topics to be updated for the school
 
     Returns:
-    True if update is successful, False otherwise
+    None
     """
-    try:
-        # Use a query to find the document with the specified school name
-        query = {"name": name}
+    # Create a new document with the updated topics
+    new_value = {"$set": {"topics": topics}}
 
-        # Update the 'topics' field with the provided list of topic
-        update = {"$set": {"topics": topics}}
-
-        # Use the update_one method to perform the update
-        result = mongo_collection.update_one(query, update)
-
-        # Check if the document was found and update
-        if result.matched_count > 0:
-            return True
-        else:
-            print(f"No school with the name '{name}' found.")
-            return False
-
-    except Exception as e:
-        print(f"Error updating topics: {e}")
-        return False
+    # Update the document in the MongoDB
+    mongo_collection.update_one({"name": name}, new_value)
